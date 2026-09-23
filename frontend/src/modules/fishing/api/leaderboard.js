64d42aead7e3c38f3ac42fd8c459097f9f48e4bd
“lihan” <lihan@66y.com>
2026-09-23 21:27:39 +0800

    chore(agent): uncommitted changes from task

--- frontend/src/modules/fishing/api/leaderboard.js
+++ frontend/src/modules/fishing/api/leaderboard.js
@@ -1,4 +1,4 @@
-import { client } from '@shared/api/client'
+import client from '../../../shared/api/client'
 
 export async function submitScore(scoreData) {
   const { data } = await client.post('/games/fishing/scores', {
